0e5f53317ada7ceae6c505e6ccf027678396d20b
Jean Albert <127751661+Albert-Jean@users.noreply.github.com>
2026-09-22 15:54:49 +0200

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+    
+        for token in tokens:
+            if token in {"+", "-", "*", "/"}:
+                # L'ordre de dépilage est crucial pour la soustraction et la division
+                b = stack.pop()
+                a = stack.pop()
+                
+                if token == '+':
+                    stack.append(a + b)
+                elif token == '-':
+                    stack.append(a - b)
+                elif token == '*':
+                    stack.append(a * b)
+                elif token == '/':
+                    # Division entière tronquée vers zéro (exigence LeetCode)
+                    stack.append(int(a / b))
+            else:
+                # C'est un nombre, on l'ajoute à la pile
+                stack.append(int(token))
+                
+        return stack[0]
+
+
+            
+            
+
+        
